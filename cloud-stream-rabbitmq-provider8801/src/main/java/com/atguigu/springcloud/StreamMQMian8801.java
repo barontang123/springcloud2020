18d7e4051ed42a5d0f37b5ece7f445b96772f9cf
admin <871217925@qq.com>
2020-04-26 17:19:57 +0800

    stream 消息队列生产和消费 group设置

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMQMian8801.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMQMian8801 {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMian8801.class,args);
+    }
+
+}
